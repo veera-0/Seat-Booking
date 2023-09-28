98150f2f593933be3b63f7112ce60aad24cfc742
Veera <valpuri52141@gmail.com>
2023-09-28 20:55:29 +0530

    added new feature

--- process.php
+++ process.php
@@ -29,13 +29,30 @@ $sql = "INSERT INTO reservations (name, roll_no, branch, year, mobile, seat) VAL
 
 //get columns to check the roll number
 $data = "SELECT roll_no from reservations";
+$result = $conn->query($data);
 
-if ($conn->query($sql) === TRUE) {
-    header("Location: result.html");
-    exit;
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+while($row = $result->fetch_assoc()){
+    $roll = $row['roll_no'];
+    if($roll == $rollno){
+        echo "Alreay seat booked through this roll no";
+        exit;
+    }
+    else if($conn->query($sql) === TRUE){
+        header("Location: result.html");
+        exit;
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
 }
+
+// if ($conn->query($sql) === TRUE) {
+//     header("Location: result.html");
+//     exit;
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
 $conn->close();
 ?>
