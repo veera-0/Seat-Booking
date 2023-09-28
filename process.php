89f3e732ae44c17fe06cc75849d1a607ec70dc62
Veera <valpuri52141@gmail.com>
2023-09-28 21:11:42 +0530

    added commands

--- process.php
+++ process.php
@@ -24,6 +24,9 @@ $mobile = $_POST['mobile'];
 $selectedSeat = $_POST['selected-seat'];
 
 
+/* create table using the following command
+    create table reservations(name varchar(100) NOT NULL,roll_no varchar(20) NOT NULL primary key,branch varchar(10) NOT NULL,year varchar(2) NOT NULL, mobile varchar(10),seat varchar(20) NOT NULL);
+*/
 
 $sql = "INSERT INTO reservations (name, roll_no, branch, year, mobile, seat) VALUES ('$name', '$rollno', '$branch', '$year', '$mobile', '$selectedSeat')";
 
