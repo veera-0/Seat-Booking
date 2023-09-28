882b8a22d77fde1cbde8f238be75dd6c2d434c7f
Veera <valpuri52141@gmail.com>
2023-09-28 20:58:42 +0530

    added error page link

--- process.php
+++ process.php
@@ -34,7 +34,7 @@ $result = $conn->query($data);
 while($row = $result->fetch_assoc()){
     $roll = $row['roll_no'];
     if($roll == $rollno){
-        echo "Alreay seat booked through this roll no";
+        header("Location: error_page.html")
         exit;
     }
     else if($conn->query($sql) === TRUE){
