8a1a93a379b5e2b3b71988afee8a714fabd3df82
Veera <valpuri52141@gmail.com>
2023-09-29 16:55:56 +0530

    seat checking added

--- process.php
+++ process.php
@@ -35,23 +35,43 @@ $year = $_POST['year'];
 $mobile = $_POST['mobile'];
 $selectedSeat = $_POST['selected-seat'];
 
+//check selected seat if its already in db
+$checkSeatQuery = "SELECT seat FROM reservations WHERE seat = ?";
+$checkSeatStmt = $conn->prepare($checkSeatQuery);
+$checkSeatStmt->bind_param("s", $selectedSeat);
+$checkSeatStmt->execute();
+$checkSeatStmt->store_result();
+
+//check roll number if its already in db
+$checkRollNoQuery = "SELECT roll_no FROM reservations WHERE roll_no = ?";
+$checkRollNoStmt = $conn->prepare($checkRollNoQuery);
+$checkRollNoStmt->bind_param("s", $rollno);
+$checkRollNoStmt->execute();
+$checkRollNoStmt->store_result();
+
+if ($checkSeatStmt->num_rows > 0) {
+    header("Location: selected_page_error.html"); 
+    exit;
+} elseif ($checkRollNoStmt->num_rows > 0) {
+    header("Location: error_page.html"); 
+    exit;
+}
+
 $sql = "INSERT INTO reservations (name, roll_no, branch, year, mobile, seat) VALUES (?, ?, ?, ?, ?, ?)";
 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ssssss", $name, $rollno, $branch, $year, $mobile, $selectedSeat);
 
+
 if ($stmt->execute()) {
-    header("Location: result.html");
+    header("Location: result.html"); 
     exit;
 } else {
-    if ($stmt->errno == 1062) { 
-        header("Location: error_page.html");
-        exit;
-    } else {
-        echo "Error: " . $stmt->error;
-    }
+    echo "Error: " . $stmt->error;
 }
 
+$checkSeatStmt->close();
+$checkRollNoStmt->close();
 $stmt->close();
 $conn->close();
 ?>
